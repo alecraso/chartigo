9bb73284a6f355a5e6d45016ce2ddb4f05f8fe85
Aaron Biller <aaron@postmates.com>
2019-05-20 16:56:23 -0400

    Add basic teams and users endpoints

--- chartigo/client.go
+++ chartigo/client.go
@@ -1,19 +1,169 @@
 package chartigo
 
-// Links represents the standard link map returned by the Chartio API
-type Links struct {
-        self     string
-        next     string
-        previous string
+import (
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "net/url"
+        "os"
+        "runtime"
+        "strings"
+        "time"
+)
+
+const (
+        // APIKeyEnvVar is the name of the environment variable where the Chartio API
+        // key should be read from.
+        APIKeyEnvVar = "CHARTIO_API_KEY"
+
+        // APIPasswordEnvVar is the name of the environment variable where the Chartio API
+        // password should be read from.
+        APIPasswordEnvVar = "CHARTIO_API_PASSWORD"
+
+        // ChartioOrgEnvVar is the name of the environment variable where the desired
+        // chartio oganization name should be read from.
+        ChartioOrgEnvVar = "CHARTIO_ORG"
+
+        // DefaultURL is the base url for the Chartio API.
+        DefaultURL = "https://api.chartio.com"
+
+        // DefaultAPIVersion is the default Chartio API version supported by this library.
+        DefaultAPIVersion = "v1"
+
+        // TimeParseFormat correctly parses timestamps returned by the Chartio API
+        TimeParseFormat
+)
+
+var (
+        // ProjectURL is the url for this library.
+        ProjectURL = "github.com/aaronbiller/chartigo"
+
+        // ProjectVersion is the version of this library.
+        ProjectVersion = "0.0.1"
+
+        // UserAgent is the user agent for this particular client.
+        UserAgent = fmt.Sprintf("ChartiGo/%s (+%s; %s)",
+                ProjectVersion, ProjectURL, runtime.Version())
+)
+
+type (
+        // Client is the main entrypoint to the Chartio golang API library.
+        Client struct {
+                BaseURL     *url.URL
+                HTTPClient  *http.Client
+                UserAgent   string
+                APIKey      string
+                APIPassword string
+                ChartioOrg  string
+        }
+
+        // NewClientInput contains the input parameters to create a new client
+        NewClientInput struct {
+                Version string
+                Org     string
+        }
+
+        // Link represents a url value returned from the Chartio API
+        Link struct {
+                href string `json:"href"`
+        }
+
+        // Links represents the standard link map returned by the Chartio API
+        Links struct {
+                self     Link `json:"self,omitempty"`
+                next     Link `json:"next,omitempty"`
+                previous Link `json:"previous,omitempty"`
+        }
+
+        // Unmarshaler interface for types that can unmarshal JSON
+        Unmarshaler interface {
+                UnmarshalJSON([]byte) error
+        }
+
+        // JSONTime type for parsing timestamps returned by the Chartio API
+        JSONTime struct {
+                time.Time
+        }
+)
+
+// UnmarshalJSON properly parses timestamps from the ChartioAPI
+func (j *JSONTime) UnmarshalJSON(b []byte) (err error) {
+        s := strings.Trim(string(b), "\"")
+        j.Time, err = time.Parse("2006-01-02T15:04:05.999999", s)
+        return
 }
 
-// APIKeyEnvVar is the name of the environment variable where the Chartio API
-// key should be read from.
-const APIKeyEnvVar = "CHARTIO_API_KEY"
+// NewClient creates a client object for querying the Chartio API
+func NewClient(i NewClientInput) *Client {
+        if i.Version == "" {
+                i.Version = DefaultAPIVersion
+        }
+        if i.Org == "" {
+                i.Org = os.Getenv(ChartioOrgEnvVar)
+        }
 
-// APIPasswordEnvVar is the name of the environment variable where the Chartio API
-// password should be read from.
-const APIPasswordEnvVar = "CHARTIO_API_PASSWORD"
+        apiKey, apiPass, err := getAPIEnvVariables()
+        if err != nil {
+                log.Fatal(err)
+        }
 
-// DefaultEndpoint is the default endpoint for Chartio.
-const DefaultEndpoint = "https://api.chartio.com"
+        URL := fmt.Sprintf("%s/%s/%s", DefaultURL, i.Version, i.Org)
+        baseURL, _ := url.Parse(URL)
+
+        client := &Client{
+                BaseURL:     baseURL,
+                HTTPClient:  http.DefaultClient,
+                UserAgent:   UserAgent,
+                APIKey:      apiKey,
+                APIPassword: apiPass,
+                ChartioOrg:  i.Org,
+        }
+        return client
+}
+
+func (c *Client) request(m, p string, v interface{}) (*http.Response, error) {
+        u := c.buildURL(p)
+
+        req, reqErr := http.NewRequest(m, u, nil)
+        if reqErr != nil {
+                return nil, reqErr
+        }
+
+        req.Header.Set("Content-Type", "application/json")
+        req.Header.Set("Accept", "application/json")
+        req.Header.Set("User-Agent", c.UserAgent)
+        req.SetBasicAuth(c.APIKey, c.APIPassword)
+
+        resp, respErr := c.HTTPClient.Do(req)
+        if respErr != nil {
+                return nil, respErr
+        }
+        defer resp.Body.Close()
+
+        jsn, jsnErr := ioutil.ReadAll(resp.Body)
+        if jsnErr != nil {
+                log.Fatal("Error reading the body", jsnErr)
+        }
+        jsnErr = json.Unmarshal(jsn, &v)
+
+        return resp, jsnErr
+}
+
+func (c *Client) buildURL(p string) string {
+        return c.BaseURL.String() + p
+}
+
+func getAPIEnvVariables() (k string, p string, err error) {
+        errMsg := "Error: environment variable %s not found, required for authentication."
+        k, kOK := os.LookupEnv(APIKeyEnvVar)
+        if !kOK {
+                return "", "", fmt.Errorf(errMsg, APIKeyEnvVar)
+        }
+        p, pOK := os.LookupEnv(APIPasswordEnvVar)
+        if !pOK {
+                return "", "", fmt.Errorf(errMsg, APIPasswordEnvVar)
+        }
+        return k, p, nil
+}
--- chartigo/team.go
+++ chartigo/team.go
@@ -1,16 +1,54 @@
 package chartigo
 
 import (
-        "time"
+        "fmt"
 )
 
-// Team represents a team object in Chartio
-type Team struct {
-        ID        int
-        Name      string
-        IsOwner   bool
-        CreatedAt time.Time
-        UpdatedAt time.Time
-        Links     Links
-        Embedded  []User
+type (
+        // Team represents a team object in Chartio
+        Team struct {
+                ID        int      `json:"id,omitempty"`
+                Name      string   `json:"name,omitempty"`
+                IsOwner   bool     `json:"is_owner,omitempty"`
+                CreatedAt JSONTime `json:"created_at,omitempty"`
+                UpdatedAt JSONTime `json:"updated_at,omitempty"`
+                Links     Links    `json:"_links,omitempty"`
+                Embedded  Users    `json:"_embedded,omitempty"`
+        }
+
+        // Teams represents an array of teams as returned by the Chartio API
+        Teams struct {
+                Teams []Team `json:"teams,omitempty"`
+                Count int    `json:"count,omitempty"`
+        }
+)
+
+// ListTeams returns the full list of teams for the current organization.
+func (c *Client) ListTeams() (*Teams, error) {
+        teams := new(Teams)
+        _, err := c.request("GET", "/teams", teams)
+        return teams, err
+}
+
+// CreateTeamInput is used as input to the CreateTeam function.
+type CreateTeamInput struct {
+        Name string
+}
+
+// CreateTeam creates a new team with the given name
+// func (c *Client) CreateTeam(i CreateTeamInput) (*Team, error) {
+//         return
+// }
+
+// GetTeamInput is used as input to the GetTeam function.
+type GetTeamInput struct {
+        ID string
+}
+
+// GetTeam returns a particular team looked up by ID
+func (c *Client) GetTeam(i GetTeamInput) (*Team, error) {
+        team := new(Team)
+        path := fmt.Sprintf("/teams/%s", i.ID)
+        _, err := c.request("GET", path, team)
+        return team, err
 }
